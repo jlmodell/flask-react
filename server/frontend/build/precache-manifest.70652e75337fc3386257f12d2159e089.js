8d7f8840ca8cf9fb0e8afbe4826970720b42ddec
jlmodell <modell.jeff@me.com>
2019-11-21 09:02:52 -0500

    commit build from server

---
+++ server/frontend/build/precache-manifest.70652e75337fc3386257f12d2159e089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928567dba7579cbfcd1f5d3b2d16f565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1dd18e2cb3f427ac28",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5a74976bb2744d30bdee",
+    "url": "/static/js/2.3d4e69e0.chunk.js"
+  },
+  {
+    "revision": "3a1dd18e2cb3f427ac28",
+    "url": "/static/js/main.52bc33a2.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
